6aa01d309c2f4ee1494c2d60b275513b43c4c21b
Julian Cox <julian.cox@webspeed.co.nz>
2024-05-03 11:26:44 +1200

    added multiviewer window plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-export { multiviewWindow } from './lib/multiview-window.js';
+import { multiviewWindow } from "./lib/multiview-window.js";
+
+export class GeocamViewerMultiviewWindow extends HTMLElement {
+  constructor() {
+    super();
+    this.plugin = null;
+    // this.yaw = this.getAttribute('yaw') || 0;
+    console.log("multiview-window init");
+  }
+
+  connectedCallback() {
+    console.log("multiview-window connected");
+    const node = this;
+    const parent = this.parentNode;
+    if (parent.viewer && parent.viewer.plugin) {
+      // Call a method on the parent
+      const target = this.getAttribute("target");
+      const element =
+        document.getElementById(target) || document.querySelector(target);
+      if (!element) {
+        console.error(`multiview-window: target '${target}' not found`);
+        return;
+      }
+      this.plugin = new multiviewWindow(element);
+      parent.viewer.plugin(this.plugin);
+    } else {
+      console.error(
+        "GeocamViewerMultiviewWindow must be a child of GeocamViewer"
+      );
+    }
+  }
+
+  disconnectedCallback() {
+    this.plugin = null;
+    console.log("multiview-window disconnected");
+    // Clean up the viewer
+  }
+}
+
+window.customElements.define(
+  "geocam-viewer-multiview-window",
+  GeocamViewerMultiviewWindow
+);
